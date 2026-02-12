f7adfb234ae4c5cdf44e68361db52786c553349e
kohhorov09 <your-email@example.com>
2026-02-12 18:47:17 +0500

    light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <Toaster position="top-center" richColors />
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
